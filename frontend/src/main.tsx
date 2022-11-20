a859f30264c22a78a0338361aee0cbf8683946fd
Eli Adelhult <hello@eli.nu>
2022-11-20 17:33:43 +0100

    Add sidebar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-import Map from './components/Map';
+import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Map />
+    <App />
   </React.StrictMode>
 );
