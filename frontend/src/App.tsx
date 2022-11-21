cd1028252f1f534458983fbacfb60c64dc3e9c39
Eli Adelhult <hello@eli.nu>
2022-11-21 17:08:40 +0100

    Created Menu component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,27 +2,14 @@ import { useState } from 'react';
 
 import Map from './components/Map';
 import Sidebar from './components/Sidebar';
+import Menu from './components/Menu';
 
 import './styles/App.css';
 
 function App() {
-  const [show, setShow] = useState(false);
-
   return (
     <div className="App">
-      <div
-        style={{
-          position: 'absolute',
-          zIndex: '1000',
-          boxSizing: 'border-box',
-          padding: '1rem',
-        }}
-      >
-        <h1>Wow cool karta</h1>
-        <button type="button" onClick={() => setShow(!show)}>
-          Click plz {show ? 'true' : 'false'}
-        </button>
-      </div>
+      <Menu />
       <Map />
       <Sidebar>
         <h3>Test</h3>
---
+++ frontend/src/components/Menu.tsx
@@ -0,0 +1,13 @@
+import '../styles/Menu.css';
+
+export default function Menu() {
+  return (
+    <div className="Menu">
+      <h1>Nice Logo™</h1>
+      <div className="intro">
+        Här är en brödtext med en intro om vad det här är. Lite om kursen och
+        hur verktyget fungerar
+      </div>
+    </div>
+  );
+}
