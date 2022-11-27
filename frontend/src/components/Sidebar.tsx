2cd0e8ec6dffefb5db6ef73db4352c16d0073932
Jonathan Widén <jonathan.loke.widen@gmail.com>
2022-11-27 19:22:17 +0100

    Added "Loading data from server..." during loading info about a stop in frontend

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import { MdClose } from 'react-icons/md';
-import { motion, AnimatePresence } from 'framer-motion';
+import {useEffect, useState} from 'react';
+import {MdClose} from 'react-icons/md';
+import {motion, AnimatePresence} from 'framer-motion';
 import {
   Bar,
   BarChart,
@@ -12,7 +12,7 @@ import {
 import Stop from '../types/Stop';
 import '../styles/Sidebar.css';
 import InfoBox from './InfoBox';
-import API, { InfoReport } from '../api';
+import API, {InfoReport} from '../api';
 
 interface SidebarProps {
   active: boolean;
@@ -20,7 +20,7 @@ interface SidebarProps {
   onClose: () => void;
 }
 
-export default function Sidebar({ active, stop, onClose }: SidebarProps) {
+export default function Sidebar({active, stop, onClose}: SidebarProps) {
   const [info, setInfo] = useState<InfoReport | null>(null);
 
   // load the info report about the stop
@@ -28,6 +28,7 @@ export default function Sidebar({ active, stop, onClose }: SidebarProps) {
     if (stop === null) return;
 
     const api = new API();
+    setInfo(null);
     api
       .infoReport(`${stop.id}`)
       .then(setInfo)
@@ -36,42 +37,46 @@ export default function Sidebar({ active, stop, onClose }: SidebarProps) {
 
   return (
     <AnimatePresence>
-      {active && stop !== null && info !== null && (
+      {active && stop !== null && (
         <motion.div
-          initial={{ translateX: '120%', translateY: -50 }}
-          animate={{ translateX: 0, translateY: 0 }}
-          exit={{ translateX: '120%', translateY: 50 }}
+          initial={{translateX: '120%', translateY: -50}}
+          animate={{translateX: 0, translateY: 0}}
+          exit={{translateX: '120%', translateY: 50}}
           className="Sidebar"
         >
           <button onClick={onClose} type="button" className="closeButton">
-            <MdClose className="closeIcon" size="1.5rem" />
+            <MdClose className="closeIcon" size="1.5rem"/>
           </button>
           <div className="content">
             <h1>{stop.name}</h1>
-            {stop.id}
-            <br />
-            <strong>avg distance:</strong> {info?.avgDistance}
-            <br />
-            <br />
-            <strong>nr. people:</strong> {info?.nrPeople}
-            <div className="infoBoxes">
-              <InfoBox color="#D7EBBA" title="Avstånd till jobbet">
-                <ResponsiveContainer width="100%" height={150}>
-                  <BarChart width={800} height={300} data={info?.distanceStats}>
-                    <XAxis dataKey="name" />
-                    <Tooltip />
-                    <YAxis /* tickFormatter={tick => `${tick}%`} */ />
-                    <Bar dataKey="distance" fill="#8884d8" />
-                  </BarChart>
-                </ResponsiveContainer>
-              </InfoBox>
-              <InfoBox
-                color="#9AD2CB"
-                title="Hur många använder den här hållplatsen"
-              >
-                <h1>20%</h1>
-              </InfoBox>
-            </div>
+            Stop ID: {stop.id}
+            <br/>
+            {info === null ? <strong>Loading data from server...</strong> : (
+              <>
+                <strong>avg distance:</strong> {info?.avgDistance}
+                <br/>
+                <br/>
+                <strong>nr. people:</strong> {info?.nrPeople}
+                <div className="infoBoxes">
+                  <InfoBox color="#D7EBBA" title="Avstånd till jobbet">
+                    <ResponsiveContainer width="100%" height={150}>
+                      <BarChart width={800} height={300} data={info?.distanceStats}>
+                        <XAxis dataKey="name"/>
+                        <Tooltip/>
+                        <YAxis /* tickFormatter={tick => `${tick}%`} */ />
+                        <Bar dataKey="distance" fill="#8884d8"/>
+                      </BarChart>
+                    </ResponsiveContainer>
+                  </InfoBox>
+                  <InfoBox
+                    color="#9AD2CB"
+                    title="Hur många använder den här hållplatsen"
+                  >
+                    <h1>20%</h1>
+                  </InfoBox>
+                </div>
+              </>
+            )}
           </div>
         </motion.div>
       )}
